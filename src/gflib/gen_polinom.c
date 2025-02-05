f377a09ee017230bc79f3f446aea08ac1a2726fd
Smail000 <shibalenkov.kir@gmail.com>
2025-02-05 14:58:24 +0300

    remove useless print

--- src/gflib/gen_polinom.c
+++ src/gflib/gen_polinom.c
@@ -10,7 +10,6 @@ polinom_t* generating_polinom(gf_t *gf, int b, int t) {
     polinom_t *polinom_res = polinom_init(2, gf);
     polinom_set(polinom_res, 0, gf_get(gf, 1+b));
     polinom_set(polinom_res, 1, gf_get(gf, 1));
-    polinom_print(polinom_res);
     for (int i = b+1; i < b+2*t; i++) {
         polinom_t *tmp = polinom_init(2, gf);
         polinom_set(tmp, 0, gf_get(gf, 1+i));
@@ -20,7 +19,6 @@ polinom_t* generating_polinom(gf_t *gf, int b, int t) {
         polinom_free(polinom_res);
         polinom_free(tmp);
         polinom_res = mult;
-        polinom_print(polinom_res);
     }
     return polinom_res;
 }
