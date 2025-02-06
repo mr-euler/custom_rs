d97c5ab48468301e8d3e544ca45c7a450ae22883
Smail000 <shibalenkov.kir@gmail.com>
2025-02-06 15:03:41 +0300

    edited coder.c (changed input form)

--- src/coder.c
+++ src/coder.c
@@ -5,26 +5,33 @@
 #include "gflib/polinom.h"
 #include "gflib/gen_polinom.h"
 
+int count_bits(int num) {
+    int count = 0;
+    while (num) {
+        num = num >> 1;
+        count++;
+    }
+    return count;
+}
+
+int decimal_to_binary(int num) {
+    int decimal = 0;
+    int mask = 1;
+    while (num) {
+        decimal += mask * (num % 10);
+        num /= 10;
+        mask = mask << 1;
+    }
+    return decimal;
+}
+
 int main() {
 
     // Параметры кода РС
 
     // Константы
 
-    int n = 7; // Количество кодовых символов
-    // Подрязумевается, что n+1 == 2^m
-
     int b = 1; // Параметр кодирования
-
-    int m = 0; // GF(q^m)
-    { // Вычисление степени поля по количеству кодовых символов
-        int tmp = n;
-        while (tmp) {
-            tmp >>= 1;
-            m++;
-        }
-
-    }
 
     // Динамические параметры
 
@@ -32,9 +39,10 @@ int main() {
     // int form_polinom = 0b1011; // x^3 + x + 1
     int form_polinom;
     printf("Введите образующий полином в 10-тичной форме.\n");
-    printf("Пример: x^3+x+1 => 0b1011 => 11\n");
+    printf("Пример: x^3+x+1 => 1011\n");
     printf(">>");
     scanf("%d", &form_polinom);
+    form_polinom = decimal_to_binary(form_polinom); // Преобразование 1011 => 11
     printf("\n"); // Отступ
 
     // Количество информационных символов
@@ -45,6 +53,11 @@ int main() {
     printf("\n"); // Отступ
 
     // Остальные параметры
+
+    int m = count_bits(form_polinom)-1; // GF(q^m)
+
+    int n = (1 << m)-1; // Количество кодовых символов
+    // Подрязумевается, что n+1 == 2^m
 
     int r = n - k; // Количество исправляющих символов
     int t = r / 2; // Количество возможных для исправления символов 
