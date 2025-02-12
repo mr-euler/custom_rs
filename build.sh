39d7c315b9f374c022098b8de5f6ee4af0a64006
galois <galois@galois.loc>
2025-02-13 01:20:55 +0300

    siplyfied build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 if  [[ "$1" =~ ^(1|main)$ ]]; then
   app=main
 elif   [[ "$1" =~ ^(2|coder)$ ]]; then
@@ -7,8 +8,5 @@ else
   echo "wrong argument"
   exit
 fi
-
-[[ -f coder ]] && rm -f coder
-[[ -f main ]] && rm -f main
-
-gcc src/gflib/gflib.c src/gflib/polinom.c src/gflib/gen_polinom.c src/main.c -o $app 
+[[ -f $app ]] && rm -f $app
+gcc src/gflib/gflib.c src/gflib/polinom.c src/gflib/gen_polinom.c src/$app.c -o $app 
